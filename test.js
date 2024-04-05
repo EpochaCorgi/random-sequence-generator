7d8a90a5c3f07811540010c09714f293bb833617
Igor Pacheco <125513732+EpochaCorgi@users.noreply.github.com>
2024-04-05 10:38:12 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-//random sequence generator
-
-let start = Math.floor(Math.random() * 10) + 1;
+// create random  integer number between 1 and 10
+let start = Math.floor(Math.random() * 10) + 1; 
 let end = Math.floor(Math.random() * 10) + 1;
 
+//create array with length equal to the difference between the two random generated numbers above, including them.
 const range = () => {
   if(start < end) {
     return Array.from({length: (end - start) + 1}, (_, i) => start + i);
